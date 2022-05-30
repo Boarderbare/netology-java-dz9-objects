f7221ba2b0f16f8ef91d314bffb5302a9636ea00
boarderbare <boarderbare@gmail.com>
2022-05-30 12:51:05 +0400

    initial

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String passporte;
+    public String phone;
+    public Boolean subscription;
+    public FormDate birthday = new FormDate();
+}
